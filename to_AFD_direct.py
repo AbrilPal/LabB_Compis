d0ddd717e114c585c4f06dcb69c7c1073175e5d5
AbrilPal <abrilpal99@gmail.com>
2023-03-18 11:38:13 -0600

    directa

--- to_AFD_direct.py
+++ to_AFD_direct.py
@@ -1,265 +1,131 @@
 from graphviz import Digraph
 
-# Definimos la clase Estado, que representa un estado del AFD
 class Estado:
-    def __init__(self, identificador, aceptacion=False):
-        self.id = identificador
-        self.aceptacion = aceptacion
+    def __init__(self, id):
+        self.id = id
         self.transiciones = {}
 
-    def agregar_transicion(self, simbolo, estado_destino):
-        if simbolo in self.transiciones:
-            self.transiciones[simbolo].append(estado_destino)
-        else:
-            self.transiciones[simbolo] = [estado_destino]
+class Automata:
+    def __init__(self):
+        self.estados = []
+        self.estado_inicial = None
+        self.estado_final = None
 
-    def obtener_transiciones(self, simbolo):
-        return self.transiciones.get(simbolo, [])
-
-    def __str__(self):
-        return str(self.id)
-
-# Definimos la clase AFD, que representa el Autómata Finito Determinístico
-class AFD:
-    def __init__(self, estados, estado_inicial, alfabeto):
-        self.estados = estados
-        self.estado_inicial = estado_inicial
-        self.alfabeto = alfabeto
-
-    def obtener_estado_por_id(self, identificador):
+    def imprimir_transiciones(self):
         for estado in self.estados:
-            if estado.id == identificador:
-                return estado
-        return None
+            print(f"Estado {estado.id}:")
+            for simbolo, estado_destino in estado.transiciones.items():
+                print(f"    {simbolo} -> Estado {estado_destino.id}")
 
-    def __str__(self):
-        return f"Estados: {[str(estado) for estado in self.estados]}\n" + \
-               f"Estado inicial: {self.estado_inicial}\n" + \
-               f"Alfabeto: {self.alfabeto}"
+def nuevo_estado(automata):
+    estado = Estado(len(automata.estados))
+    automata.estados.append(estado)
+    return estado
+
+def automata_simbolo(automata, pila, simbolo):
+    estado_inicial = nuevo_estado(automata)
+    estado_final = nuevo_estado(automata)
+    estado_inicial.transiciones[simbolo] = estado_final
+    pila.append((estado_inicial, estado_final))
+
+def automata_concatenacion(automata, pila):
+    estado2_inicial, estado2_final = pila.pop()
+    estado1_inicial, estado1_final = pila.pop()
+    estado1_final.transiciones[''] = estado2_inicial
+    pila.append((estado1_inicial, estado2_final))
+
+def automata_alternativa(automata, pila):
+    estado_inicial2, estado_final2 = pila.pop()
+    estado_inicial1, estado_final1 = pila.pop()
+    estado_nuevo_inicial = nuevo_estado(automata)
+    estado_nuevo_final = nuevo_estado(automata)
+    estado_nuevo_inicial.transiciones[''] = estado_inicial1
+    estado_nuevo_inicial.transiciones[''] = estado_inicial2
+    estado_final1.transiciones[''] = estado_nuevo_final
+    estado_final2.transiciones[''] = estado_nuevo_final
+    pila.append((estado_nuevo_inicial, estado_nuevo_final))
+
+def automata_repeticion(automata, pila):
+    estado_inicial, estado_final = pila.pop()
+    estado_nuevo_inicial = nuevo_estado(automata)
+    estado_nuevo_final = nuevo_estado(automata)
+    estado_nuevo_inicial.transiciones[''] = estado_inicial
+    estado_nuevo_inicial.transiciones[''] = estado_nuevo_final
+    estado_final.transiciones[''] = estado_inicial
+    estado_final.transiciones[''] = estado_nuevo_final
+    pila.append((estado_nuevo_inicial, estado_nuevo_final))
+
+def automata_repeticion_positiva(automata, pila):
+    estado_inicial, estado_final = pila.pop()
+    estado_nuevo_inicial = nuevo_estado(automata)
+    estado_nuevo_final = nuevo_estado(automata)
+    estado_nuevo_inicial.transiciones[''] = estado_inicial
+    estado_final.transiciones[''] = estado_inicial
+    estado_final.transiciones[''] = estado_nuevo_final
+    pila.append((estado_nuevo_inicial, estado_nuevo_final))
+
+def automata_opcional(automata, pila):
+    estado_inicial, estado_final = pila.pop()
+    estado_nuevo_inicial = nuevo_estado(automata)
+    estado_nuevo_final = nuevo_estado(automata)
+    estado_nuevo_inicial.transiciones[''] = estado_inicial
+    estado_nuevo_inicial.transiciones[''] = estado_nuevo_final
+    estado_final.transiciones[''] = estado_nuevo_final
+    pila.append((estado_nuevo_inicial, estado_nuevo_final))
 
 
-def construir_AFD(expresion_regular):
-    # Definimos una función auxiliar para crear nuevos estados
-    contador_estados = 0
-    def nuevo_estado():
-        nonlocal contador_estados
-        estado = Estado(contador_estados)
-        contador_estados += 1
-        return estado
 
-    # Definimos una función auxiliar para crear transiciones vacías
-    def transicion_vacia(estado_origen, estado_destino):
-        estado_origen.agregar_transicion("", estado_destino)
 
-    # Definimos una función auxiliar para agregar la clausura vacía de un estado
-    def agregar_clausura_vacia(estado, visitados):
-        visitados.add(estado)
-        for destino in estado.obtener_transiciones(""):
-            if destino not in visitados:
-                agregar_clausura_vacia(destino, visitados)
-    
-    # Definimos una función auxiliar para agregar una transición a un estado a partir de un símbolo
-    def agregar_transicion(estado_origen, estado_destino, simbolo):
-        if simbolo in afd.alfabeto:
-            estado_origen.agregar_transicion(simbolo, estado_destino)
-
-    # Definimos las variables necesarias para construir el AFD
-    afd = AFD([], None, set())
+def construir_automata(expresion_regular):
+    automata = Automata()
     pila = []
-    visitados = set()
 
-    # Recorremos la expresión regular
     for simbolo in expresion_regular:
-        if simbolo == "(":
-            pila.append("(")
-        elif simbolo == ")":
-            while pila and pila[-1] != "(":
-                operando2 = pila.pop()
-                operando1 = pila.pop()
-                # Agregamos una transición vacía desde el primer operando al segundo
-                transicion_vacia(operando1[1], operando2[0])
-                # Unimos los alfabetos de ambos operandos
-                afd.alfabeto |= operando1[2] | operando2[2]
-                # Agregamos ambos operandos a la pila
-                pila.append((operando1[0], operando2[1], afd.alfabeto.copy()))
-            pila.pop() # Sacamos el paréntesis de la pila
-        elif simbolo == '?':
-            # Obtenemos el último operando que está en la pila
-            operando = pila.pop()
-            # Creamos un nuevo estado inicial y otro de aceptación
-            estado_inicial = nuevo_estado()
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado del operando
-            transicion_vacia(estado_inicial, operando[0])
-            # Agregamos una transición vacía desde el primer estado del operando hacia el estado de aceptación
-            transicion_vacia(operando[0], estado_aceptacion)
-            # Agregamos una transición vacía desde el estado inicial hacia el estado de aceptación
-            transicion_vacia(estado_inicial, estado_aceptacion)
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((estado_inicial, estado_aceptacion, set()))
+        if simbolo == '|':
+            automata_alternativa(automata, pila)
+        elif simbolo == '.':
+            automata_concatenacion(automata, pila)
         elif simbolo == '*':
-            # Obtenemos el último operando que está en la pila
-            operando = pila.pop()
-            # Creamos un nuevo estado inicial y otro de aceptación
-            estado_inicial = nuevo_estado()
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado del operando
-            transicion_vacia(estado_inicial, operando[0])
-            # Agregamos una transición vacía desde el último estado del operando hacia el estado de aceptación
-            transicion_vacia(operando[1], estado_aceptacion)
-            # Agregamos una transición vacía desde el último estado del operando hacia el primer estado del operando
-            transicion_vacia(operando[1], operando[0])
-            # Agregamos una transición vacía desde el estado inicial hacia el estado de aceptación
-            transicion_vacia(estado_inicial, estado_aceptacion)
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((estado_inicial, estado_aceptacion, set()))
+            automata_repeticion(automata, pila)
         elif simbolo == '+':
-            # Obtenemos el último operando que está en la pila
-            operando = pila.pop()
-            # Creamos un nuevo estado inicial y otro de aceptación
-            estado_inicial = nuevo_estado()
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado del operando
-            transicion_vacia(estado_inicial, operando[0])
-            # Agregamos una transición vacía desde el último estado del operando hacia el estado de aceptación
-            transicion_vacia(operando[1], estado_aceptacion)
-            # Agregamos una transición vacía desde el último estado del operando hacia el primer estado del operando
-            transicion_vacia(operando[1], operando[0])
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((operando[0], estado_aceptacion, set()))
-        elif simbolo == '.':
-            # Obtenemos los últimos dos operandos que están en la pila
-            operando2 = pila.pop()
-            operando1 = pila.pop()
-            # Agregamos las transiciones desde el
-            estado_inicial = nuevo_estado()
-            # Creamos un nuevo estado de aceptación
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado del operando
-            transicion_vacia(estado_inicial, operando[0])
-            # Agregamos una transición vacía desde el último estado del operando hacia el estado de aceptación
-            transicion_vacia(operando[1], estado_aceptacion)
-            # Agregamos una transición vacía desde el estado inicial hacia el estado de aceptación
-            transicion_vacia(estado_inicial, estado_aceptacion)
-            # Agregamos una transición vacía desde el último estado del operando hacia el primer estado del operando
-            transicion_vacia(operando[1], operando[0])
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((estado_inicial, estado_aceptacion, set()))
-        elif simbolo == '+':
-            # Obtenemos el último operando que está en la pila
-            operando = pila.pop()
-            # Creamos un nuevo estado inicial
-            estado_inicial = nuevo_estado()
-            # Creamos un nuevo estado de aceptación
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado del operando
-            transicion_vacia(estado_inicial, operando[0])
-            # Agregamos una transición vacía desde el último estado del operando hacia el estado de aceptación
-            transicion_vacia(operando[1], estado_aceptacion)
-            # Agregamos una transición vacía desde el último estado del operando hacia el primer estado del operando
-            transicion_vacia(operando[1], operando[0])
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((estado_inicial, estado_aceptacion, set()))
-        elif simbolo == '|':
-            # Obtenemos los dos últimos operandos que están en la pila
-            operando2 = pila.pop()
-            operando1 = pila.pop()
-            # Creamos un nuevo estado inicial
-            estado_inicial = nuevo_estado()
-            # Creamos un nuevo estado de aceptación
-            estado_aceptacion = nuevo_estado()
-            # Agregamos una transición vacía desde el estado inicial hacia el primer estado de cada operando
-            transicion_vacia(estado_inicial, operando1[0])
-            transicion_vacia(estado_inicial, operando2[0])
-            # Agregamos una transición vacía desde el último estado de cada operando hacia el estado de aceptación
-            transicion_vacia(operando1[1], estado_aceptacion)
-            transicion_vacia(operando2[1], estado_aceptacion)
-            # Unimos los alfabetos de ambos operandos
-            afd.alfabeto |= operando1[2] | operando2[2]
-            # Agregamos una marca a la pila para indicar que hay un nuevo operando
-            pila.append((estado_inicial, estado_aceptacion, afd.alfabeto.copy()))
-        elif simbolo == '.':
-            # Obtenemos los dos últimos operandos que están en la pila
-            operando2, visitados2 = pila.pop()
-            operando1, visitados1 = pila.pop()
-            # Creamos una nueva expresión regular que representa la concatenación de los operandos
-            nueva_expresion = operando1 + operando2
-            # Unimos los conjuntos de estados visitados de ambos operandos
-            visitados = visitados1 | visitados2
-            # Agregamos la clausura vacía del primer operando
-            agregar_clausura_vacia(operando1, visitados)
-            # Agregamos el alfabeto del primer operando
-            afd.alfabeto |= operando1.alfabeto
-            # Agregamos el nuevo operando a la pila
-            pila.append((nueva_expresion, visitados))
-        else: # Si no es ningún símbolo especial, es un símbolo del alfabeto
-            afd.alfabeto.add(simbolo)
-            if not pila:
-                print("hola")
-                estado_inicial = nuevo_estado()
-                estado_aceptacion = nuevo_estado()
-                # Agregamos una transición desde el estado inicial hacia el estado de aceptación
-                agregar_transicion(estado_inicial, estado_aceptacion, simbolo)
-                # Agregamos una marca a la pila para indicar que hay un nuevo operando
-                pila.append((estado_inicial, estado_aceptacion, set()))
-            else:
-                print("no")
-                # Obtenemos el último operando que está en la pila
-                operando = pila.pop()
-                # Creamos un nuevo estado inicial y otro de aceptación
-                estado_inicial = nuevo_estado()
-                estado_aceptacion = nuevo_estado()
-                # Agregamos una transición desde el estado inicial hacia el estado de aceptación
-                agregar_transicion(estado_inicial, estado_aceptacion, simbolo)
-                # Agregamos una transición desde el estado inicial hacia el primer estado del operando
-                transicion_vacia(estado_inicial, operando[0])
-                # Agregamos una transición vacía desde el último estado del operando hacia el estado de aceptación
-                transicion_vacia(operando[1], estado_aceptacion)
-                # Agregamos una transición vacía desde el último estado del operando hacia el primer estado del operando
-                transicion_vacia(operando[1], operando[0])
-                # Agregamos una marca a la pila para indicar que hay un nuevo operando
-                pila.append((estado_inicial, estado_aceptacion, set()))
+            automata_repeticion_positiva(automata, pila)
+        elif simbolo == '?':
+            automata_opcional(automata, pila)
+        else:
+            automata_simbolo(automata, pila, simbolo)
 
+    while len(pila) > 1:
+        automata_concatenacion(automata, pila)
 
-    # Al salir del ciclo, sólo debe haber un operando en la pila
-    operando = pila.pop()
-    # Agregamos la clausura vacía del operando
-    agregar_clausura_vacia(operando[0], visitados)
-    # Agregamos el alfabeto del operando
-    afd.alfabeto |= operando[2]
-    # Agregamos el estado inicial, los estados de aceptación y el alfabeto al AFD
-    afd.estado_inicial = operando[0]
-    afd.estados_aceptacion = visitados
-    afd.alfabeto = sorted(list(afd.alfabeto))
-    return afd
+    automata.estado_inicial = pila[0][0]
+    automata.estado_final = pila[0][1]
 
+    return automata
 
+def simular_automata(automata, cadena):
+    estado_actual = automata.estado_inicial
+    for simbolo in cadena:
+        if simbolo not in estado_actual.transiciones:
+            return False
+        estado_actual = estado_actual.transiciones[simbolo]
+    return estado_actual == automata.estado_final
 
-# Definimos la función que dibuja el AFD utilizando Graphviz
-def dibujar_AFD(afd):
-    g = Digraph()
-
-    # Dibujamos los estados
-    for estado in afd.estados:
-        g.node(str(estado), shape='circle')
-        if estado.aceptacion:
-            g.node(str(estado) + "_", shape='doublecircle')
-
-    # Dibujamos las transiciones
-    for estado in afd.estados:
-        for simbolo in afd.alfabeto:
-            destinos = estado.obtener_transiciones(simbolo)
-            for destino in destinos:
-                g.edge(str(estado), str(destino), label=simbolo)
-
-    # Dibujamos la flecha hacia el estado inicial
-    g.node("inicio", shape='none')
-    g.edge("inicio", str(afd.estado_inicial), arrowhead='none')
-
-    # Mostramos el grafo
+def graficar_automata(automata):
+    g = Digraph('G', filename='automata.gv')
+    g.attr(rankdir='LR', size='8,5')
+    g.attr('node', shape='doublecircle')
+    g.node(str(automata.estado_final.id))
+    g.attr('node', shape='circle')
+    g.node(str(automata.estado_inicial.id))
+    g.attr('node', shape='circle')
+    for estado in automata.estados:
+        for simbolo, estado_destino in estado.transiciones.items():
+            g.edge(str(estado.id), str(estado_destino.id), label=simbolo)
     g.view()
 
-# Ejemplo de uso
-expresion_regular = "(a|b)*(b|a)*abb"
-afd = construir_AFD(expresion_regular)
-dibujar_AFD(afd)
+expresion_regular = '(a|b)*(b|a)*abb'
+automata = construir_automata(expresion_regular)
+automata.imprimir_transiciones()
+print(simular_automata(automata, 'abb')) 
+print(simular_automata(automata, 'aaaaaaaaaabbbbbbabababababababababababababbb')) 
+graficar_automata(automata)
